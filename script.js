bf1bc948d244ce72bb21340922cc6a6ee81d36d4
Max Meischke <meischkem@outlook.com>
2022-03-18 10:35:21 -0400

    Added function to clear grid and prompt for size

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 // Var for default grid size
 let gridSize = 16;
-
 // Reference each div in page
 const header = document.querySelector('.header');
 const btnWrapper = document.querySelector('.btnWrapper');
 const gridWrapper = document.querySelector('.gridWrapper');
 
+// clearGrid function will clear and rebuild the grid depending on size prompt
+const clearGrid = function(){
+    const newGridSize = prompt('Please enter the desired grid size (Max. 64): ', '');
+    if (newGridSize>64){
+        alert('Please enter a size of 64 or less.');
+    }
+    else{
+        gridWrapper.innerHTML='';
+        createGrid(newGridSize);
+    };
+};
+
 // Button to clear grid
 const clearBtn = document.createElement('button');
 clearBtn.classList.add('clearBtn');
-clearBtn.textContent="Clear";
+clearBtn.textContent='Clear';
+clearBtn.addEventListener('click',clearGrid);
 
 //Button to switch to "rainbow" coloring
 const rainbowBtn = document.createElement('button');
 rainbowBtn.classList.add('rainbowBtn');
-rainbowBtn.textContent="Rainbow"
+rainbowBtn.textContent='Rainbow';
 
 // add buttons to page (inside btnWrapper)
 btnWrapper.appendChild(clearBtn);
@@ -30,11 +42,11 @@ const createGrid = function(gridSize){
             const gridCol = document.createElement('div');
             gridCol.classList.add('gridCol');
             gridRow.appendChild(gridCol); 
-            gridCol.addEventListener("mouseover", function(gridCol){
+            gridCol.addEventListener('mouseover', function(gridCol){
                 gridCol.target.style.backgroundColor="black";
             });
-        }
-    }
+        };
+    };
 };
 
 createGrid(gridSize);
